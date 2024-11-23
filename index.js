ff1dcfd7400ceacaa0f28e99b3b6ccf0bbf40ab5
WisnuDwiSeisno <wseisno37@gmail.com>
2024-11-23 19:12:06 +0700

    memulai kembali

--- index.js
+++ index.js
@@ -1,57 +1,9 @@
-// Definisi Library yang digunakan
-require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
 
-
-const express = require("express");
-const session = require("express-session");
-const bodyParser = require("body-parser");
-const path = require("path");
-const flash = require("req-flash");
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-// Definisi lokasi file router
-const contactRoutes = require("./src/routes/router-bibit");
-const appRoutes = require("./src/routes/router-app");
 
-// Configurasi library session
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: "4734",
-    name: "akashi",
-    cookie: {
-      sameSite: true,
-      maxAge: 60000,
-    },
-  })
-);
-
-// Configurasi dan gunakan library
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(flash());
-
-app.use(function (req, res, next) {
-  res.setHeader(
-    "Cache-Control",
-    "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0"
-  );
-  res.setHeader("Pragma", "no-cache");
-  next();
-});
-
-// Setting folder views
-app.set("views", path.join(__dirname, "src/views"));
-app.set("view engine", "ejs");
-
-// Gunakan routes yang telah didefinisikan
-app.use("/bibit", contactRoutes);
-app.use("/", appRoutes);
-
-console.log(app._router.stack);
-
-// Gunakan port server
-app.listen(8080, () => {
-  console.log("Server Berjalan di Port : " + 8080);
-});
+app.listen(5000, () => console.log('Server listening on port'));
--- src/routes/router-register.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const registerController = require('../controllers').register;
-require('dotenv').config();
-
-
-router.get('/', registerController.formRegister);
-router.post('/save', registerController.saveRegister);
-
-module.exports = router;
